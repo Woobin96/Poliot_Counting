530c354b9209a06b0cce15984058614ff85be796
wbkong <hmy1219@naver.com>
2025-08-04 15:01:24 +0900

    1. 재현평가 text 추가

--- src/string.js
+++ src/string.js
@@ -2,7 +2,7 @@ export const STRINGS = {
     HEADER_TITLE: '공수 카운팅',
     FullTC: "FullTC",
     Result: "개선 확인",
-    VOC: "재현 평가 (VOC 및 고객사 요청)",
+    VOC: "재현 평가 (VOC 및 고객사 요청,타 브랜드 재현 코멘트 작성 시)",
     TC_Update : "TC 업데이트",
 
     Up: "+",
