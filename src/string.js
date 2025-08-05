8154360d7e0e746caceffe25addf35ecda35ec0d
wbkong <hmy1219@naver.com>
2025-08-05 14:11:10 +0900

    1. 타이틀 변경

--- src/string.js
+++ src/string.js
@@ -3,10 +3,11 @@ export const STRINGS = {
     FullTC: "FullTC",
     Result: "개선 확인",
     VOC: "재현 평가 (VOC 및 고객사 요청,타 브랜드 재현 코멘트 작성 시)",
-    TC_Update : "TC 업데이트",
+    TC_Update: "TC 업데이트",
 
     Up: "+",
-    Reset:"전체 초기화",
-    Save : "저장",
-  };
-  
+    Reset: "전체 초기화",
+    Save: "저장",
+    Made: "만든이",
+    Kong: "Kong & Park",
+};
